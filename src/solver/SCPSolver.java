07c3a97d541b8d56392f743c798657bc249beda9
Steven Goron <pedrogrn860@gmail.com>
2020-02-26 17:29:32 +0100

    Debut des contraintes

--- src/solver/SCPSolver.java
+++ src/solver/SCPSolver.java
@@ -16,11 +16,17 @@ import input.ProblemInstance;
 import input.ProblemInstance.OwnerDesire;
 import model.ResourceOwner;
 import model.User;
+import model.UserGroup;
+import scpsolver.constraints.LinearBiggerThanEqualsConstraint;
+import scpsolver.constraints.LinearEqualsConstraint;
+import scpsolver.constraints.LinearSmallerThanEqualsConstraint;
 import scpsolver.lpsolver.LinearProgramSolver;
 import scpsolver.lpsolver.SolverFactory;
 import scpsolver.problems.LinearProgram;
 
 public class SCPSolver implements ISolver {
+	static Map<String , Integer> NameToId = new HashMap<>();
+	
 	public static Set<UserResourceInstanceAllocation> getOptimalAllocation(ProblemInstance input) {
 	
 		int maximumInsatisfaction = 
@@ -130,6 +136,7 @@ public class SCPSolver implements ISolver {
 				users,
 				inF.getRelativeInsatisfactionFor(allAdmissibleAllocations),
 				allocToVar, varPerOwner);
+		System.out.println(NameToId);
 		
 		LinearProgram lp = new LinearProgram(weights);
 		for (int i = 0; i < weights.length; i++) {
@@ -139,16 +146,8 @@ public class SCPSolver implements ISolver {
 		
 		lp.setMinProblem(true);
 		
-		System.out.println(lp.convertToCPLEX());
-		
-		LinearProgramSolver solver  = SolverFactory.newDefault();
-		System.out.println(solver);
-		//double[] sol = solver.solve(lp);
-		
-		/*
-		
 		generateAllocationConstraints(
-				cplex,
+				lp,
 				allAdmissibleAllocations,
 				allAdmissibleResourceInstances,
 				allocToVar,
@@ -158,6 +157,18 @@ public class SCPSolver implements ISolver {
 				inF, maxNbResourcePerOwner
 				);
 		
+		
+		
+		
+		System.out.println(lp.convertToCPLEX());
+		
+		LinearProgramSolver solver  = SolverFactory.newDefault();
+		System.out.println(solver);
+		//double[] sol = solver.solve(lp);
+		
+		
+		
+		/*
 		//lp.convertToCPLEX();
 
 		if (! cplex.solve() )
@@ -186,10 +197,56 @@ public class SCPSolver implements ISolver {
 		Map<UserResourceInstanceAllocation, String> varPerAlloc = new HashMap<>();
 		
 		
-		for(UserResourceInstanceAllocation pl : consideredAllocations)
-				varPerAlloc.put(pl, pl.toString());
+		for(UserResourceInstanceAllocation pl : consideredAllocations) {
+			varPerAlloc.put(pl, pl.toString());
+			NameToId.put(pl.toString(),NameToId.size());
+		}
 		
 		return varPerAlloc;
+	}
+	
+	public static void generateAllocationConstraints(
+			LinearProgram lp, 
+			Set<UserResourceInstanceAllocation> allAdmissibleAllocations, 
+			SortedSet<ResourceInstance> allAdmissibleResources,
+			Map<UserResourceInstanceAllocation, String> varPerAlloc,
+			Map<ResourceInstance, String> allocatedResourceVar,
+			Map<ResourceOwner, String> varPerActiveOwner, 
+			Map<ResourceInstance, String> allocationJoker,
+			ProblemInstance inF,
+			int maxNbResourcePerOwner
+			) {
+		
+		connectResourcesAndAllocations(lp,
+				inF, 
+				allocatedResourceVar,
+				varPerAlloc
+				);
+
+		matchAllocationsOfGroups(lp, inF, varPerAlloc);
+		
+		Set<UserResourceInstanceAllocation>validAllocations = varPerAlloc.keySet();
+		for(User pl: inF.getAllUsers())
+		{
+			double weights[] = new double[NameToId.size()];
+			for(UserResourceInstanceAllocation al: inF.getResouceInstanceAllocationsFor(pl))
+			{
+				if(!validAllocations.contains(al))continue;
+				int id  = NameToId.get(varPerAlloc.get(al));
+				weights[id] = 1.0;
+			}
+			
+			lp.addConstraint(new LinearEqualsConstraint(weights, 1.0,"EachUserIsGivenExactlyOneResource("+pl+")"));
+			
+		}
+		
+		allocateEachResourceInstanceAtMostKTimes(lp, 
+				inF, 
+				varPerAlloc, 
+				allAdmissibleResources,
+				validAllocations);
+
+
 	}
 	
 	private static Map<ResourceInstance, String> getVarsPerResource(Set<ResourceInstance> allAdmissibleResourceInstances) {
@@ -197,6 +254,7 @@ public class SCPSolver implements ISolver {
 		
 		for(ResourceInstance r: allAdmissibleResourceInstances)
 		{
+			NameToId.put("ActiveResourceVar("+r+")",NameToId.size());
 			res.put(r, "ActiveResourceVar("+r+")");
 		}
 		return res;
@@ -211,9 +269,10 @@ public class SCPSolver implements ISolver {
 		.map(x->inF.getOwner(x.getResource()))
 		.collect(Collectors.toSet());
 		
-		for(ResourceOwner r: ro)
+		for(ResourceOwner r: ro) {
+			NameToId.put("ResourceOwnerHasAtLeastOneResourceTaken("+r+")",NameToId.size());
 			res.put(r, "ResourceOwnerHasAtLeastOneResourceTaken("+r+")");
-		
+		}
 		return res;
 	}
 	
@@ -278,6 +337,96 @@ public class SCPSolver implements ISolver {
 		
 		return weightsArray;
 	}
+	
+	private static void connectResourcesAndAllocations(LinearProgram lp ,ProblemInstance inF,
+			Map<ResourceInstance, String> allocatedResourceVar, 
+			Map<UserResourceInstanceAllocation, String> varPerAlloc) 
+	{
+		for(ResourceInstance r: allocatedResourceVar.keySet())
+		{
+			
+			Set<UserResourceInstanceAllocation> allocationsForResource = 
+					varPerAlloc.keySet()
+					.stream()
+					.filter(x->x.getResource().equals(r))
+					.collect(Collectors.toSet());
+					
+			double weights[] = new double[NameToId.size()];
+			for(UserResourceInstanceAllocation ua:allocationsForResource) {
+				int id =  NameToId.get(varPerAlloc.get(ua));
+				weights[id] = 1.0;
+			}
+			
+			//IloNumExpr resource = 
+					//cplex.prod(allocationsForResource.size(), allocatedResourceVar.get(r));
+			int id = NameToId.get(allocatedResourceVar.get(r));
+			weights[id] = -allocationsForResource.size();
+			
+			lp.addConstraint(new LinearSmallerThanEqualsConstraint(weights, 0.0,"AllocationsMakeResourceConsummed("+r+")"));
+
+		}
+	}
+	
+	private static void matchAllocationsOfGroups(LinearProgram lp, ProblemInstance inF,
+			Map<UserResourceInstanceAllocation, String> varPerAlloc
+			){
+		
+		Map<UserGroup,Map<ResourceInstance,String>> varPerResourcePerGroup = 
+				new HashMap<>();
+
+		for(UserGroup ug: inF.getUserGroups())
+		{
+			
+			varPerResourcePerGroup.put(ug, new HashMap<>());
+			for(ResourceInstance r: 
+				varPerAlloc.keySet()
+				.stream()
+				.filter(x->ug.getUsers().contains(x.getUser()))
+				.map(x->x.getResource())
+				.collect(Collectors.toSet())
+					)
+			{
+				NameToId.put("isResourceAllocatedToGroup("+r+","+ug+")", NameToId.size());
+				varPerResourcePerGroup.get(ug).put(r,
+						"isResourceAllocatedToGroup("+r+","+ug+")");
+			}
+			double weights[] = new double[NameToId.size()];
+			for(UserResourceInstanceAllocation ua:
+				varPerAlloc.keySet().stream()
+				.filter(x->ug.getUsers().contains(x.getUser()))
+				.collect(Collectors.toSet()))
+			{
+				int id = NameToId.get(varPerResourcePerGroup.get(ug).get(ua.getResource()));
+				weights[id] = -1.0;
+				id =  NameToId.get(varPerAlloc.get(ua));
+				weights[id] = 1.0;
+				lp.addConstraint(new LinearEqualsConstraint(weights, 0.0,"allMemberOfUserGroupsMustBeAllocatedTheSameResource("+ua+","+ug+")"));
+			}
+		}
+	}
+	
+	private static void allocateEachResourceInstanceAtMostKTimes(
+			LinearProgram lp, ProblemInstance inF,
+			Map<UserResourceInstanceAllocation, String> varPerAlloc,
+			Set<ResourceInstance> allAdmissibleResources,
+			Set<UserResourceInstanceAllocation> allAdmissibleAllocations){
+		for(ResourceInstance resource: allAdmissibleResources)
+		{
+			double weights[] = new double[NameToId.size()];
+			for(UserResourceInstanceAllocation ua: inF.getAllocationsForResource(resource)
+					.stream()
+					.filter(x->allAdmissibleAllocations.contains(x))
+					.collect(Collectors.toSet()))
+			{
+				int id = NameToId.get(varPerAlloc.get(ua));
+				weights[id] = 1.0;
+			}
+			
+			lp.addConstraint(new LinearSmallerThanEqualsConstraint(weights,inF.getMaxNbUsersPerResource(),"EachResourceIsAllocatedAtMostKTimes("+resource+","+inF.getMaxNbUsersPerResource()+")"));
+		}
+	}
+	
+	
 
 	
 }
